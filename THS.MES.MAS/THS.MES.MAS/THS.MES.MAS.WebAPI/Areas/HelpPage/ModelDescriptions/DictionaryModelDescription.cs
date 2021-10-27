a7b51eff1ab5b620eb471dfe3f238b211a544e39
ChengSueYun <57125794+ChengSueYun@users.noreply.github.com>
2021-10-27 15:57:43 +0800

    Delete THS.MES.MAS/THS.MES.MAS directory

--- THS.MES.MAS/THS.MES.MAS/THS.MES.MAS.WebAPI/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace THS.MES.MAS.WebAPI.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
--- THS.MES.MAS/THS.MES.MAS/THS.MES.MAS.WebAPI/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="THS.MES.MAS.WebAPI.WebApiApplication" Language="C#" %>
